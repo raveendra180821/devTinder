6ac447efaad567f79f358791e87530523f6a7fac
raveendra180821 <raveendra180821@gmail.com>
2025-11-04 14:17:34 +0530

    Stop tracking database.js and add to .gitignore

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-    await mongoose.connect(
-        "mongodb+srv://mamu:Mamu%40180821@mamu.hwlvsq4.mongodb.net/devTinder"
-    )
-}
-
-module.exports = connectDB
